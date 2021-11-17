85aa8a96eda5a256aa718b6fe44d324d8749da55
Siarhei Karukhin <siarhei_karukhin@epam.com>
2021-11-17 11:16:23 +0300

    feat: add tests for converter

--- src/modem/__tests__/converter.spec.js
+++ src/modem/__tests__/converter.spec.js
@@ -1,9 +1,13 @@
 import converter from '../converter.js';
 
-// test('it should works ;D', () => {
-//   expect(converter(1)('abc')).toBe('bcd');
-// });
+test('It should convert with a positive offset', () => {
+  expect(converter(1)('abc')).toBe('bcd');
+});
 
-test('it should works ;D', () => {
-  expect(1+1).toBe(2);
+test('It should convert with a negative offset', () => {
+  expect(converter(-1)('abc')).toBe('zab');
+});
+
+test('It should only convert the Latin alphabet', () => {
+  expect(converter(2)('abc123абв')).toBe('cde123абв');
 });
