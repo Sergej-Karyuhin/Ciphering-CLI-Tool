83c10b00313715d9bac3b0f1cd0bfbc9a5ad0eed
Siarhei Karukhin <siarhei_karukhin@epam.com>
2021-11-17 10:37:19 +0300

    feat: add jest

---
+++ src/modem/__tests__/converter.spec.js
@@ -0,0 +1,9 @@
+import converter from '../converter.js';
+
+// test('it should works ;D', () => {
+//   expect(converter(1)('abc')).toBe('bcd');
+// });
+
+test('it should works ;D', () => {
+  expect(1+1).toBe(2);
+});
